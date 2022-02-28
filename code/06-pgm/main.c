53ed585c10e7bd84feace224a85faaf8e415c038
Kcpf <fernandopmgf@al.insper.edu.br>
2022-02-28 11:16:31 -0300

    adicionando lab 6

--- code/06-pgm/main.c
+++ code/06-pgm/main.c
@@ -28,6 +28,37 @@ int limiar(int level, int limiar) {
     return 255;
 }
 
+int crop(int altura, int largura, struct imagem *imagem_original, struct imagem *imagem_cortada) {
+    if (altura > imagem_original->altura || largura > imagem_original->largura) return 1; 
+
+    imagem_cortada->altura = altura;
+    imagem_cortada->largura = largura;
+    imagem_cortada->max_pixel = imagem_original->max_pixel;
+
+    for (int i = 0; i < largura; i++) {
+        for (int j = 0; j < altura; j++) {
+            imagem_cortada->matrix[i][j] = imagem_original->matrix[i][j];   
+        }
+    }
+
+    return 0;
+}
+
+int blur(struct imagem *imagem_original, struct imagem *imagem_blur) {
+    imagem_blur->altura = imagem_original->altura;
+    imagem_blur->largura = imagem_original->largura;
+    imagem_blur->max_pixel = imagem_original->max_pixel;
+
+    for (int i = 0; i < imagem_original->largura; i++) {
+        for (int j = 0; j < imagem_original->altura; j++) {
+            if ()
+            imagem_cortada->matrix[i][j] = imagem_original->matrix[i][j];   
+        }
+    }
+
+    return 0;
+}
+
 void escreve_imagem(struct imagem *imagem) {
     for (int i = 0; i < imagem->largura; i++) {
         for (int j = 0; j < imagem->altura; j++) {
@@ -47,11 +78,16 @@ void print_imagem(struct imagem *imagem) {
 int main() {
 
     struct imagem imagem;
+    struct imagem imagem_cortada;
     le_imagem(&imagem);
 
     escreve_imagem(&imagem);
 
-    print_imagem(&imagem);    
+    // print_imagem(&imagem);
+
+    crop(20, 20, &imagem, &imagem_cortada);
+
+    print_imagem(&imagem_cortada);
 
     return 0;
 }
